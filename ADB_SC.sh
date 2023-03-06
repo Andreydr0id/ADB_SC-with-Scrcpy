2df977bf368606a030ea2ba34df8ce9b0a5962d1
Andreydr0id <110672262+Andreydr0id@users.noreply.github.com>
2023-03-06 11:50:33 +0400

    Add files via upload

---
+++ ADB_SC.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+adb kill-server
+adb tcpip 5555 && sleep 3
+ipadr=$(adb shell ip -o a | grep "wlan0" | head -n1 | awk '{print $4}' | awk '{ print substr($1, 1, length($1)-3) '}) #получение IP
+if [[ $ipadr == "" ]]; then
+notify-send "ADB_SC" "Failed to get ip"
+else
+adb connect $ipadr:5555 # подключение телефона через TCP
+notify-send "ADB_SC" "Success"
+scrcpy --stay-awake --serial=$ipadr:5555
+fi
